de77327e5badf88612a786d6978a429d371230d7
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2021-10-25 22:30:49 -0500

    Index view tickets

--- templates/Tickets/index.php
+++ templates/Tickets/index.php
@@ -1,50 +1,39 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Ticket[]|\Cake\Collection\CollectionInterface $tickets
- */
-?>
-<div class="tickets index content">
-    <?= $this->Html->link(__('New Ticket'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Tickets') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('status') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th><?= $this->Paginator->sort('user_id') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($tickets as $ticket): ?>
-                <tr>
-                    <td><?= $this->Number->format($ticket->id) ?></td>
-                    <td><?= h($ticket->status) ?></td>
-                    <td><?= h($ticket->created) ?></td>
-                    <td><?= h($ticket->modified) ?></td>
-                    <td><?= $ticket->has('user') ? $this->Html->link($ticket->user->name, ['controller' => 'Users', 'action' => 'view', $ticket->user->id]) : '' ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $ticket->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $ticket->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $ticket->id], ['confirm' => __('Are you sure you want to delete # {0}?', $ticket->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+<div class="row">
+    <div class="col-12 d-flex">
+        <div class="card flex-fill table-responsive">
+            <div class="card-header">
+                <h5 class="card-title mb-0">Tickets</h5>
+            </div>
+            <table class="table table-hover my-0">
+                <thead>
+                    <tr>
+                        <th><?= $this->Paginator->sort('id') ?></th>
+                        <th><?= $this->Paginator->sort('user_id') ?></th>
+                        <th><?= $this->Paginator->sort('status') ?></th>
+                        <th><?= $this->Paginator->sort('ticket') ?></th>
+                        <th><?= $this->Paginator->sort('created') ?></th>
+                        <th><?= $this->Paginator->sort('modified') ?></th>
+                        <th class="actions"><?= __('Actions') ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($tickets as $ticket): ?>
+                        <tr>
+                            <td><?= $this->Number->format($ticket->id) ?></td>
+                            <td><?= $ticket->has('user') ? $this->Html->link($ticket->user->name, ['controller' => 'Users', 'action' => 'view', $ticket->user->id]) : '' ?></td>
+                            <td><?= h($ticket->status) ? '<span class="badge bg-success">Cerrado</span>' : '<span class="badge bg-danger">Abierto</span>' ?></td>
+                            <td><?= h($ticket->ticket) ?></td>
+                            <td><?= h($ticket->created) ?></td>
+                            <td><?= h($ticket->modified) ?></td>
+                            <td class="actions">
+                                <?= $this->Html->link(__('View'), ['action' => 'view', $ticket->id]) ?>
+                                <?= $this->Html->link(__('Edit'), ['action' => 'edit', $ticket->id]) ?>
+                                <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $ticket->id], ['confirm' => __('Are you sure you want to delete # {0}?', $ticket->id)]) ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
